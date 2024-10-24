78778dd09cfc866a23c0a56cf6c464a7e3da06aa
TheDeeGaming <donrwr@gmail.com>
2024-10-24 21:56:15 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './../App.css';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h2>Nachho m apka Swagat h</h2>
+      <p>Yaha ake sab nacho</p>
+      <button className="hero-button">Get Started</button>
+    </div>
+  );
+};
+
+export default Hero;
